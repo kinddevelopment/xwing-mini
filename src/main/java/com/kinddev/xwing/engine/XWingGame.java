ad29274d52066ebf04c1c178c2eec449b76b6f9c
Markus Svensson <markus.sve@gmail.com>
2016-06-28 21:56:04 +0200

    Added IntelliJ project with Maven setup. Added source files with simple test to test the build process with Travis CI.

---
+++ src/main/java/com/kinddev/xwing/engine/XWingGame.java
@@ -0,0 +1,14 @@
+package com.kinddev.xwing.engine;
+
+/**
+ * Created by markus on 2016-06-28.
+ */
+public class XWingGame {
+    public static int Adder(int a, int b) {
+        return a + b;
+    }
+
+    public static int Subtractor(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/test/java/com/kinddev/xwing/engine/XWingGameTest.java
@@ -0,0 +1,15 @@
+package com.kinddev.xwing.engine;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by markus on 2016-06-28.
+ */
+public class XWingGameTest {
+    @org.junit.Test
+    public void adder() throws Exception {
+        assertTrue(XWingGame.Adder(5,2) == 7);
+
+    }
+
+}
